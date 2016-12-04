7d450e836ec54503f61534860f178fae1e4b6932
Aet Udusaar <aet.udusaar@gmail.com>
2016-12-04 11:06:00 +0000

    lab3 pooleli

---
+++ src/hmi_msg.h
@@ -0,0 +1,13 @@
+#ifndef _HMI_MSG_H_
+#define _HMI_MSG_H_
+
+#define STUD_NAME "Aet Udusaar"
+const char *ENG_MONTH[6] = {"January",
+"February",
+"March",
+"April",
+"May",
+"June",};
+
+#endif /*_HMI_MSG_H_*/
+
--- src/main.c
+++ src/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-#define __ASSERT_USE_STDERR
-#include <assert.h>
 #include <avr/io.h>
 #include <util/delay.h>
 #include "uart.h"
+#include "print_helper.h"
+#include "hmi_msg.h"
+
 
 #define BLINK_DELAY_MS 100
 
@@ -13,35 +14,32 @@ void main (void)
     DDRA |= _BV(DDA3);
     /* Init error console as stderr in UART3 and print user code info */
     uart3_init();
+    stdout = stdin = &uart0_io;
     stderr = &uart3_out;
     fprintf(stderr, "Version: %s built on: %s %s\n",
             GIT_DESCR, __DATE__, __TIME__);
     fprintf(stderr, "avr-libc version: %s\n", __AVR_LIBC_VERSION_STRING__);
     /* End UART3 init and info print */
-    /* Test assert - REMOVE IN FUTURE LABS */
-    char *array;
-    uint32_t i = 1;
-    extern int __heap_start, *__brkval;
-    int v;
-    array = malloc( i * sizeof(char));
-    assert(array);
-    /* End test assert */
+    fprintf(stdout, STUD_NAME "\n");
+    /* ASCII table print */
+    print_ascii_tbl(stdout);
+    unsigned char ascii[128] = {0};
+
+    for (unsigned char i = 0; i < sizeof(ascii); i++) {
+        ascii[i] = i;
+    }
+
+    print_for_human(stdout, ascii, 128);
 
     while (1) {
         /* Set pin 3 high to turn LED on */
         PORTA |= _BV(PORTA3);
         _delay_ms(BLINK_DELAY_MS);
-        /* Test assert - REMOVE IN FUTURE LABS */
-        /*
-         * Increase memory allocated for array by 100 chars
-         * until we have eaten it all and print space between Stack and Heap.
-         * Thats how assert works in run-time.
-         */
-        array = realloc( array, (i++ * 100) * sizeof(char));
-        fprintf(stderr, "%d ",
-                (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval));
-        assert(array);
-        /* End test assert */
+        /* Month name comparison and print */
+        char inBuf = ' ';
+        fprintf(stdout, "Enter Month name first letter >");
+        fscanf(stdin, "%c", &inBuf);
+        fprintf(stdout, "%c\n", inBuf);
         /* Set pin 3 low to turn LED off */
         PORTA &= ~_BV(PORTA3);
         _delay_ms(BLINK_DELAY_MS);
