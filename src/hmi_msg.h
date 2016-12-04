02eae42c789e01d2e66d76ce026d95338a8ab70d
Aet Udusaar <aet.udusaar@gmail.com>
2016-12-04 20:59:35 +0000

    lab 3 valmis

--- src/hmi_msg.h
+++ src/hmi_msg.h
@@ -2,12 +2,16 @@
 #define _HMI_MSG_H_
 
 #define STUD_NAME "Aet Udusaar"
-const char *ENG_MONTH[6] = {"January",
-"February",
-"March",
-"April",
-"May",
-"June",};
+#define GET_MONTH_NAME "Enter Month name first letter >"
+#define PRG_VER "Version: %s built on: %s %s"
+#define LIBC_VER "avr-libc version: %s avr-gcc version: %s"
+const char month1[]  PROGMEM = "January";
+const char month2[] PROGMEM = "February";
+const char month3[]    PROGMEM = "March";
+const char month4[]    PROGMEM = "April";
+const char month5[]      PROGMEM = "May";
+const char month6[]     PROGMEM = "June";
 
+PGM_P const months[] PROGMEM = {month1,month2,month3,month4,month5,month6};
 #endif /*_HMI_MSG_H_*/
 
--- src/main.c
+++ src/main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <avr/io.h>
+#include <avr/pgmspace.h>
 #include <util/delay.h>
+#include <string.h>
+#include "hmi_msg.h"
 #include "uart.h"
 #include "print_helper.h"
-#include "hmi_msg.h"
+#include "../lib/hd44780_111/hd44780.h"
 
 
 #define BLINK_DELAY_MS 100
@@ -13,33 +16,50 @@ void main (void)
     /* Set pin 3 of PORTA for output */
     DDRA |= _BV(DDA3);
     /* Init error console as stderr in UART3 and print user code info */
-    uart3_init();
+    uart0_initialize();
+    uart3_initialize();
     stdout = stdin = &uart0_io;
     stderr = &uart3_out;
-    fprintf(stderr, "Version: %s built on: %s %s\n",
-            GIT_DESCR, __DATE__, __TIME__);
-    fprintf(stderr, "avr-libc version: %s\n", __AVR_LIBC_VERSION_STRING__);
+    lcd_init();
+    lcd_clrscr();
+    fprintf_P(stderr, PSTR(PRG_VER "\n"),
+              GIT_DESCR, __DATE__, __TIME__);
+    fprintf_P(stderr, PSTR(LIBC_VER "\n"), __AVR_LIBC_VERSION_STRING__,
+              __VERSION__);
     /* End UART3 init and info print */
-    fprintf(stdout, STUD_NAME "\n");
+    fprintf_P(stdout, PSTR(STUD_NAME "\n"));
+    lcd_puts_P(PSTR(STUD_NAME));
     /* ASCII table print */
     print_ascii_tbl(stdout);
-    unsigned char ascii[128] = {0};
+    unsigned char ascii_table[128] = {0};
 
-    for (unsigned char i = 0; i < sizeof(ascii); i++) {
-        ascii[i] = i;
+    for (unsigned char i = 0; i < sizeof(ascii_table); i++) {
+        ascii_table[i] = i;
     }
 
-    print_for_human(stdout, ascii, 128);
+    print_for_human(stdout, ascii_table, 128);
 
     while (1) {
         /* Set pin 3 high to turn LED on */
         PORTA |= _BV(PORTA3);
         _delay_ms(BLINK_DELAY_MS);
         /* Month name comparison and print */
-        char inBuf = ' ';
-        fprintf(stdout, "Enter Month name first letter >");
-        fscanf(stdin, "%c", &inBuf);
-        fprintf(stdout, "%c\n", inBuf);
+        char letter;
+        fprintf_P(stdout, PSTR(GET_MONTH_NAME));
+        fscanf(stdin, "%c", &letter);
+        fprintf(stdout, "%c\n", letter);
+        lcd_goto(0x40);
+
+        for (int i = 0; i < 6; i++) {
+            if (!strncmp_P(&letter, (PGM_P)pgm_read_word(&months[i]), 1)) {
+                fprintf_P(stdout, (PGM_P)pgm_read_word(&months[i]));
+                fputc('\n', stdout);
+                lcd_puts_P((PGM_P)pgm_read_word(&months[i]));
+                lcd_putc(' ');
+            }
+        }
+
+        lcd_puts_P(PSTR("                "));
         /* Set pin 3 low to turn LED off */
         PORTA &= ~_BV(PORTA3);
         _delay_ms(BLINK_DELAY_MS);
